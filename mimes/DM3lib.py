396651dc1cb522ca64c621feff61715ead71e479
dgursoy <sparsedata@gmail.com>
2015-01-28 15:54:26 -0600

    typo corrected

--- mimes/DM3lib.py
+++ mimes/DM3lib.py
@@ -8,7 +8,7 @@ Pierre-Ivan Raynal <raynal@univ-tours.fr>
 
 Contributions by:
 Philippe Mallet-Ladeira <philippe.mallet@cemes.fr>
-Doga Gursoy <dgursoy@aps.anl.gov
+Doga Gursoy <dgursoy@aps.anl.gov>
 
 This Python module is a transposition and adaptation of the DM3_Reader 
 ImageJ plug-in (http://rsb.info.nih.gov/ij/plugins/DM3_Reader.html) 
